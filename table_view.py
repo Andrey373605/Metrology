7dcf928a8e3bdad52892c8b5c9e808117c620945
35350139 <zarkazaz@gmail.com>
2025-02-28 10:11:07 +0300

    Table_View таблица доделана

--- table_view.py
+++ table_view.py
@@ -3,6 +3,7 @@ from operands_func import *
 import tkinter as tk
 from tkinter import ttk
 
+
 def main():
     root = tk.Tk()
     root.title("Метрика Холстеда")
@@ -11,26 +12,28 @@ def main():
         content = file.read()
 
     tree1 = ttk.Treeview(root, columns=("Операнд", "Количество"), show="headings")
-    tree1.heading("Операнд", text="Количество")
-    tree1.heading("Операнд", text="Количество")
+    tree1.heading("Операнд", text="Операнд")
+    tree1.heading("Количество", text="Количество")
 
     data1 = find_operands(content)
 
     for item in data1:
         tree1.insert("", "end", values=item)
 
-
     tree2 = ttk.Treeview(root, columns=("Оператор", "Количество"), show="headings")
-    tree2.heading("Оператор", text="Количество")
-    tree2.heading("Оператор", text="Количество")
+    tree2.heading("Оператор", text="Оператор")
+    tree2.heading("Количество", text="Количество")
 
     data2 = find_operators(content)
 
     for item in data2:
         tree2.insert("", "end", values=item)
 
-
     tree1.pack(side="left", fill="both", expand=True)
     tree2.pack(side="right", fill="both", expand=True)
 
     root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
